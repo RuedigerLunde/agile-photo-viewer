d7be998c460e88abe6a57d5e7f9a8ec659c3e122
RuedigerLunde <Ruediger.Lunde@gmail.com>
2016-01-19 11:46:04 +0100

    - Documentation updated

--- agile-photo-viewer/AgilePhotoViewerFXStarter.cmd
+++ agile-photo-viewer/AgilePhotoViewerFXStarter.cmd
@@ -1,3 +1,3 @@
-rem allocates heap space, starts the Agile Photo Viewer (Swing Version) and makes error messages visible
+rem allocates heap space, starts the Agile Photo Viewer (FX Version) and makes error messages visible
 
 java -Xmx1500M -jar AgilePhotoViewerApp.jar
